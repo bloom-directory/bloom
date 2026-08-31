9319c49c97bb3ec95b91b4f4acf6ddfb7ab60427
0xdewy <15720036+0xdewy@users.noreply.github.com>
2026-08-30 17:48:48 -0700

    fix(vfs): align Solana status with genesis config rename

--- crates/bloom-vfs/src/handlers/wallets.rs
+++ crates/bloom-vfs/src/handlers/wallets.rs
@@ -6679,7 +6679,7 @@ mod tests {
                     max_rps: None,
                     http_only: false,
                 }],
-                expected_genesis_hex: Some("test-genesis".into()),
+                expected_genesis_base58: Some("test-genesis".into()),
                 allow_broadcast: false,
             })
             .unwrap(),
@@ -6870,7 +6870,7 @@ mod tests {
                     max_rps: None,
                     http_only: false,
                 }],
-                expected_genesis_hex: Some("test-genesis".into()),
+                expected_genesis_base58: Some("test-genesis".into()),
                 allow_broadcast: false,
             })
             .unwrap(),
