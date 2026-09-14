5162b487a4ead388a03f5581dfcb6d99f52b29bd
Gavin Pacini <gavin.pacini@gmail.com>
2026-09-14 16:55:58 +0200

    Update Broker API and Petal contract to master heads

--- packaging/triad/release/verify-bundle.sh
+++ packaging/triad/release/verify-bundle.sh
@@ -113,7 +113,7 @@ signer_revision="$(source_revision BLOOM_SIGNER_SHA)"
   exit 65
 }
 require_compat_value revisions service_runtime_commit '"bc88cc6760b00bbff0c6a6e5f56d42bb03004436"'
-require_compat_value revisions petal_contract_commit '"9ad7a5c635f092bbcdf8f00a06b23676806ff70c"'
+require_compat_value revisions petal_contract_commit '"73c5b06a77599368fbc79fb7947a629b5b4c630e"'
 for state_owner in machine broker signer; do
   require_compat_value "state.$state_owner" current 1
   require_compat_value "state.$state_owner" downgrade_floor 1
